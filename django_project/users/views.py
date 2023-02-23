e8e5d8c6eed3f3e3e819b57555c5a589c288cd5f
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-02-23 10:02:45 +0000

    users can now updat thier profile details

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages 
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserUpdateForm, ProfileUpdateForm
 
 
 def register(request):
@@ -19,4 +19,20 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        u_update_form = UserUpdateForm(request.POST, instance=request.user)
+        p_update_form = ProfileUpdateForm(request.POST,request.FILES, instance=request.user.profile)
+        if u_update_form.is_valid() and p_update_form.is_valid():
+            u_update_form.save()
+            p_update_form.save()
+            messages.success(request,f'Your Account has been updated!')
+            return redirect('profile')
+    else:
+        u_update_form = UserUpdateForm(instance=request.user)
+        p_update_form = ProfileUpdateForm(instance=request.user.profile)
+    context = {
+        'u_update_form': u_update_form,
+        'p_update_form': p_update_form
+    }
+    
+    return render(request,'users/profile.html', context)
