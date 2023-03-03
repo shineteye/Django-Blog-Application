be105c2d19b753d3f8bde68e070d78a74d93baca
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-03-03 15:07:06 +0000

    enabled list, view and CRUD posts

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -3,12 +3,16 @@ from . import views
 from .views import (
     PostListView, 
     PostDetailView, 
-    PostCreateView)
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView)
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     # path('', views.home, name='blog-home'), this comes from using the functional view
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
